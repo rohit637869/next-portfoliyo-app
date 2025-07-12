ac2357aabfefdd43cb0fb8029eee18135c44fe5c
Rohit verma <132906863+rohit637869@users.noreply.github.com>
2025-07-12 10:59:23 +0530

    Solving

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,13 +1,9 @@
 "use client"
 import Blogs from "@/components/personal/Blogs";
-import { useLinkStatus } from "next/link";
-import { useParams, useSearchParams } from "next/navigation";
 
 export default function page() {
 
-    let parms = useSearchParams()
-    console.log(parms.get('p'))
-    const { pending } = useLinkStatus()
+    
     return  pending ? (
     <div role="status" aria-label="Loading" className="spinner" />
   ) :(
