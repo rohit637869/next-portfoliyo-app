6cb93c6f5b967f150e5f62ecb794a449b4f85a8c
Rohit verma <132906863+rohit637869@users.noreply.github.com>
2025-07-12 11:08:20 +0530

    READy

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -4,9 +4,7 @@ import Blogs from "@/components/personal/Blogs";
 export default function page() {
 
     
-    return  pending ? (
-    <div role="status" aria-label="Loading" className="spinner" />
-  ) :(
+    return  (
         <>
             <div className="my-10"></div>
             <Blogs blogs={[1, 2, 3, 4, 65, 7, 8, 9]} />
