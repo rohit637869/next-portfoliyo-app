d14de96a532c8c60e88367566e8dfb1b9ce292ac
Rohit verma <132906863+rohit637869@users.noreply.github.com>
2025-07-12 11:23:53 +0530

    READy

--- components/personal/Navbar.jsx
+++ components/personal/Navbar.jsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
         <div className=" bg-muted">
             <nav className="z-50 fixed top-6 inset-x-4 h-16 max-w-screen-xl mx-auto rounded-full border border-slate-700/70 bg-white/30 dark:bg-slate-900/30 backdrop-blur-sm">
                 <div className="h-full flex items-center justify-between mx-auto px-4">
-                    <h1 className="text-2xl text-orange-500 px-3">RXMAT</h1>
+                    <h1 className="text-2xl text-orange-500 px-3 font-bold">RXMAT</h1>
                     {/* Desktop Menu */}
                     <NavMenu className="hidden md:block" />
                     <div className="flex items-center gap-3">
